8a500f9fbc66c37d37b2c03f307aaca4a33e2815
LeepDev <hyungy@gmail.com>
2023-09-18 11:53:36 -0500

    Updated Course model to include default par

--- models/course.js
+++ models/course.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
   name: { type: String, required: true },
-  parTotal: { type: Number, required: true },
+  parTotal: { type: Number, required: true, default: 72 },
   parOut: { type: Number },
   parIn: { type: Number },
   holePars: [{ type: Number }],
